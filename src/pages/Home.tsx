0c346ebf6fe82fd969a833df88cf7c20d800ffe5
SahebRana <m.sahebrana@gmail.com>
2025-02-11 19:48:27 +0900

    setup: initial ui setup completed

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+  const title: string = "Prayer Time";
+
+  return (
+    <div class="w-full flex justify-center items-center p-2">
+      <div class="max-w-lg min-w-3xs flex flex-col gap-y-4 justify-start items-start p-2">
+        <h1 class="text-lg text-slate-900 font-semibold">{title}</h1>
+      </div>
+    </div>
+  )
+}
